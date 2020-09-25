a63dbd22c41b757591d0df1f79e07c7ee3bb5ae4
Igor Olenchuk <Olenchuk.io@yandex.ru>
2020-09-25 22:56:26 +0300

    Attempt 1

--- homework.py
+++ homework.py
@@ -1,21 +1,32 @@
 import time
-
+import os
 import requests
+from dotenv import load_dotenv
 from twilio.rest import Client
 
+load_dotenv()
 
 def get_status(user_id):
     params = {
-        ...
+        'user_ids': user_id,
+        'fields': 'online',
+        'v': 5.92,
+        'access_token': os.getenv('access_token')
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
-
+    get_status = requests.post('https://api.vk.com/method/users.get', params=params)
+    return get_status.json()['response'][0]['online']  # Верните статус пользователя в ВК
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    account_sid = os.getenv('account_sid')
+    auth_token = os.getenv('auth_token')
+    client = Client(account_sid, auth_token)
+    sms_text = client.messages.create(
+        to=os.getenv('NUMBER_TO'),
+        from_=os.getenv('NUMBER_FROM'),
+        body="Hello from Python!")
+    return sms_text.sid # Верните sid отправленного сообщения из Twilio
 
+# Download the helper library from https://www.twilio.com/docs/python/install
 
 if __name__ == "__main__":
     vk_id = input("Введите id ")

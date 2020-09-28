9c8b6c0f14bcd36e5c792530f495b9463bf81625
Igor Olenchuk <Olenchuk.io@yandex.ru>
2020-09-28 22:07:28 +0300

    after review 1

--- homework.py
+++ homework.py
@@ -6,24 +6,34 @@ from twilio.rest import Client
 
 load_dotenv()
 
+api_version = 5.9
+TOKEN = os.getenv('access_token')
+account_sid = os.getenv('account_sid')
+auth_token = os.getenv('auth_token')
+url_base = 'https://api.vk.com/method/users.get'
+client = Client(account_sid, auth_token)
+
 def get_status(user_id):
     params = {
         'user_ids': user_id,
         'fields': 'online',
-        'v': 5.92,
-        'access_token': os.getenv('access_token')
+        'v': api_version,
+        'access_token': TOKEN
     }
-    get_status = requests.post('https://api.vk.com/method/users.get', params=params)
-    return get_status.json()['response'][0]['online']  # Верните статус пользователя в ВК
+    try:
+        get_status = requests.post(url_base, params=params)
+    except NameError:
+        print("Нет такой страницы")
+    try:
+        return get_status.json()['response'][0]['online']  # Верните статус пользователя в ВК
+    except ValueError:
+        print("Такого пользователя не существует")
 
 def sms_sender(sms_text):
-    account_sid = os.getenv('account_sid')
-    auth_token = os.getenv('auth_token')
-    client = Client(account_sid, auth_token)
     sms_text = client.messages.create(
         to=os.getenv('NUMBER_TO'),
         from_=os.getenv('NUMBER_FROM'),
-        body="Hello from Python!")
+        body=sms_text)
     return sms_text.sid # Верните sid отправленного сообщения из Twilio
 
 # Download the helper library from https://www.twilio.com/docs/python/install
